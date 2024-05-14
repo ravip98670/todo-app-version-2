5088f6c88c747f5019aec39aeed3670a48ad3d2b
ravip98670 <ravispandey2002@gmail.com>
2024-05-15 00:49:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import { useState } from "react";
+
+function App() {
+  const initialTodoItems = [];
+
+  const [todoItems, setTodoItems] = useState(initialTodoItems);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+  return (
+    <center className="todo-container">
+      <AppName />
+
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,5 @@
+function WelcomeMessage(){
+    return <p>Enjoy Your Day..</p>
+}
+
+export default WelcomeMessage;
